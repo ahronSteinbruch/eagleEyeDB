73d512ac4f16db46126a7c4f9b8913850e4138f0
ahronSteinbruch <139044285+ahronSteinbruch@users.noreply.github.com>
2025-06-09 11:42:08 +0300

    change the enum location to a specific file

---
+++ Status.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sqlComands
+{
+    //status for status type agent
+    public enum Status
+    {
+        Active,
+        Injured,
+        Missing,
+        Retired
+    }
+}
--- obj/Debug/net9.0/sqlComands.AssemblyInfo.cs
+++ obj/Debug/net9.0/sqlComands.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sqlComands")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8bcc3120e016ca02eb06b99b56636404a927f20")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a9b3d1c39c267299306506dfb340eb9e3902bea")]
 [assembly: System.Reflection.AssemblyProductAttribute("sqlComands")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sqlComands")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
